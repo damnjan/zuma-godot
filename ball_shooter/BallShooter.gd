442bcb54b69d77d835d46a55aa7d1396586ebfce
Damnjan Luković <damnjan@Damnjans-MacBook-Pro.local>
2023-06-28 02:04:00 +0200

    ball shooter visual improvements

--- ball_shooter/BallShooter.gd
+++ ball_shooter/BallShooter.gd
@@ -24,59 +24,40 @@ func _ready():
 	tongue.returned.connect(_on_tongue_returned)
 	tongue.collided.connect(_on_tongue_collided)
 	
-func check_collision_with_follows(object: Node2D, self_radius: float, callback: Callable):
-	for group in group_manager.groups:
-		for ball in group.items:
-			if ball.is_hidden:
-				continue
-			if object.global_position.distance_to(ball.global_position) < Globals.BALL_WIDTH / 2 + self_radius:
-				callback.call(ball)
-				return
-	
 func ray_to_ball_intersection(ray_origin: Vector2, ray_direction: Vector2):
 	var closest_intersection = null
 	var closest_distance = INF
 	var ball_radius = Globals.BALL_WIDTH / 2  # Assuming you have a property for the ball's radius
-#	for group in group_manager.groups:
 	for ball in get_tree().get_nodes_in_group('visible_balls'):
-		if ball.is_hidden:
-			continue
-			
 		var ball_position: Vector2 = ball.global_position
 		var origin_to_ball: Vector2 = ball_position - ray_origin
-		
 		# Projection of the ball's position onto the ray direction
 		var projection_length = origin_to_ball.dot(ray_direction)
 		if projection_length < 0:
 			continue
-
 		# Closest point on the ray to the center of the circle
 		var closest_point = ray_origin + ray_direction * projection_length
-
 		var distance_to_ball_center = closest_point.distance_to(ball_position)
-
-		if distance_to_ball_center < ball_radius:
+		if distance_to_ball_center < ball_radius * 2:
 			# The ray intersects the ball.
 			# Distance from the closest point on the line to the intersection
-			var distance_to_intersection = sqrt(ball_radius * ball_radius - distance_to_ball_center * distance_to_ball_center)
-			
+			var distance_to_intersection = sqrt(pow(ball_radius * 2, 2) - pow(distance_to_ball_center,2 ))
 			var intersection_point = closest_point - ray_direction* distance_to_intersection
 			var distance = ray_origin.distance_to(intersection_point)
-
 			if distance < closest_distance:
 				closest_distance = distance
 				closest_intersection = intersection_point
-	
 	return closest_intersection
 
 func _physics_process(_delta):
-	var mouse_rotation = Vector2.UP.angle_to(get_local_mouse_position().normalized())
+	var mouse_rotation = Vector2.UP.angle_to(get_local_mouse_position())
 	var collision_point =  ray_to_ball_intersection(global_position, get_local_mouse_position().normalized())
 	var point_position = collision_point if collision_point else shooting_direction * 4000
+	var polygon_width = Globals.BALL_WIDTH
 	var local_point_position = to_local(point_position)
 	if mode == Modes.NORMAL:
 		polygon_2d.visible = !!shooting_ball
-	polygon_2d.polygon = [Vector2(0, -local_point_position.length() - polygon_2d.position.y), Vector2(-30, 0), Vector2(30, 0)]
+	polygon_2d.polygon = [Vector2(0, -local_point_position.length() - polygon_2d.position.y), Vector2(-polygon_width/2, 0), Vector2(polygon_width/2, 0)]
 	toad.rotation = mouse_rotation
 	if shooting_ball:
 		shooting_ball.global_position = spawn_point.global_position
@@ -85,12 +66,11 @@ func _physics_process(_delta):
 		polygon_2d.color = Color(1,1,1,0.5)
 
 
-
 func _input(event):
 	if OS.is_debug_build():
 		var keys = [KEY_1, KEY_2, KEY_3, KEY_4]
 		for i in keys.size():
-			if Input.is_key_pressed(keys[i]):
+			if Input.is_key_pressed(keys[i]) and shooting_ball:
 				shooting_ball.ball.frame = i
 	
 	if event.is_action_pressed("shoot") and shooting_ball:
